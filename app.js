edff9b901f27db3010a1a30bf43f7cbd64f81e5c
daniyal ali <daniyalali12568@gmail.com>
2025-02-22 00:30:10 +0500

    add_projects

---
+++ app.js
@@ -0,0 +1,267 @@
+ {/* <div className="bg-blue-50">
+                <div className="app_container p-4">
+
+                <ProductFeatureTimeline
+                headingText={'Your enterprise HR lifecycle tools'}
+                headingTextClasses='font-bold text-900 text-6xl mb-3 text-center'
+                headingDescription={'Transform your HR functions and processes with our best in-class tools'}
+                headingDescriptionClasses='text-700 text-xl text-center mb-5 line-height-3'
+                sectionClasses='surface-section px-4 py-8 md:px-6 lg:px-8 overflow-hidden'
+                features={[
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-1.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-blue-500',
+                        featureLineColor: 'bg-blue-500',
+                        featureClasses: 'lg:justify-content-center mb-5',
+                        featureTitle: 'Plan & Hire',
+                        featureSubTitle: 'Quisque sagittis purus sit amet volutpat consequat mauris nunc. Scelerisque eleifend.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Sed lectus vestibulum</span> mattis ullamcorper velit. Laoreet sit amet cursus sit.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Fames ac turpis</span> egestas sed tempus urna et. Cursus turpis massa.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-2.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-yellow-500',
+                        featureLineColor: 'bg-yellow-500',
+                        featureClasses: 'justify-content-center mb-5',
+                        featureTitle: 'Onboard',
+                        featureSubTitle: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Quis hendrerit dolor</span> magna eget est. Pellentesque pulvinar pellentesque.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Lectus urna duis</span> convallis convallis tellus id interdum velit laoreet.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-3.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-cyan-500',
+                        featureLineColor: 'bg-cyan-500',
+                        featureClasses: 'justify-content-center mb-5',
+                        featureTitle: 'Training & development',
+                        featureSubTitle: 'Id interdum velit laoreet id. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Egestas dui id</span> ornare arcu odio ut. Mi bibendum neque egestas congue.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Sed velit dignissim</span> sodales ut eu. Massa placerat duis ultricies lacus.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-1.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-blue-500',
+                        featureLineColor: 'bg-blue-500',
+                        featureClasses: 'lg:justify-content-center mb-5',
+                        featureTitle: 'Engage',
+                        featureSubTitle: 'Quisque sagittis purus sit amet volutpat consequat mauris nunc. Scelerisque eleifend.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Sed lectus vestibulum</span> mattis ullamcorper velit. Laoreet sit amet cursus sit.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Fames ac turpis</span> egestas sed tempus urna et. Cursus turpis massa.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-2.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-yellow-500',
+                        featureLineColor: 'bg-yellow-500',
+                        featureClasses: 'justify-content-center mb-5',
+                        featureTitle: 'Retention',
+                        featureSubTitle: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Quis hendrerit dolor</span> magna eget est. Pellentesque pulvinar pellentesque.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Lectus urna duis</span> convallis convallis tellus id interdum velit laoreet.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        image: {
+                            imageSrc: '/demo/images/blocks/feature/feature-timeline-3.png',
+                            imageAlt: 'Image',
+                            imageClasses: 'w-full mr-8'
+                        },
+                        featureCircleTextColor: '',
+                        featureCircleBackgroundColor: 'bg-cyan-500',
+                        featureLineColor: 'bg-cyan-500',
+                        featureClasses: 'justify-content-center mb-5',
+                        featureTitle: 'Reporting',
+                        featureSubTitle: 'Id interdum velit laoreet id. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.',
+                        featureDetailItems: [
+                            {
+                                entries: [
+                                    {
+                                        item: '<div className="mb-2 line-height-3"><span className="text-900 font-medium">Egestas dui id</span> ornare arcu odio ut. Mi bibendum neque egestas congue.</div>'
+                                    },
+                                    {
+                                        item: '<div className="line-height-3"><span className="text-900 font-medium">Sed velit dignissim</span> sodales ut eu. Massa placerat duis ultricies lacus.</div>'
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]}
+            />
+                    <GridListCardWithActions
+                    headingText='Your enterprise HR lifecycle tools'
+                    subHeadingText='Transform your HR functions and processes with our best in-class tools'
+                        items={[
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Planning & Hiring',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-green-100',
+                                iconClasses:'text-green-600',
+                                animateClass:'fadeInLeft',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-success',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Onboarding',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-yellow-100',
+                                iconClasses:'text-yellow-600',
+                                animateClass:'fadeIn',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-warning',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Development & Performance',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-purple-100',
+                                iconClasses:'text-purple-600',
+                                animateClass:'fadeInRight',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-help',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Engage',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-green-100',
+                                iconClasses:'text-green-600',
+                                animateClass:'fadeInLeft',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-success',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Retention',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-yellow-100',
+                                iconClasses:'text-yellow-600',
+                                animateClass:'fadeIn',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-warning',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            {
+                                containerClasses: 'col-12 lg:col-4 p-3',
+                                title: 'Reporting',
+                                subTitle: 'Quam adipiscing vitae proin sagittis.',
+                                description: 'Eget sit amet tellus cras adipiscing enim. At quis risus sed vulputate odio. Proin libero nunc consequat interdum varius sit amet.',
+                                iconBgClasses:'bg-purple-100',
+                                iconClasses:'text-purple-600',
+                                animateClass:'fadeInRight',
+                                button: {
+                                    label: 'More',
+                                    icon: 'pi pi-arrow-right',
+                                    iconPos: 'right',
+                                    classNames: 'p-button-rounded p-button-help',
+                                    actionUrl:'/hr/hire'
+                                }
+                            },
+                            
+                        ]}
+                    />
+                </div>
+            </div> */}
